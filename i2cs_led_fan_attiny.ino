51d26983374e7b5f55faff6bb02364617d9c656f
Osamu Mizuno <o-mizuno@kit.ac.jp>
2016-03-28 22:29:00 +0900

    dim interval change. read led value

--- i2cs_led_fan_attiny.ino
+++ i2cs_led_fan_attiny.ino
@@ -1,24 +1,38 @@
+/*
+ * LED and Fan controller
+ *
+ * I2C write:
+ *     pin # (D000000X), value (0 - 255), dim interval (0-255)[sec]
+ *            |      +- pin # (0,1)
+ *            +-------- dim flag (for pin 0)
+ * I2C read:
+ *     returns the value of pin #
+ */
+
 #include <TinyWireS.h>              // Requires fork by Rambo with onRequest support
 
-#define LED_PIN 1
-#define FAN_PIN 4
+#define LED_PIN (1)
+#define FAN_PIN (4)
 
-const int I2CSlaveAddress = 0x27;      // I2C Address.
+#define SLAVE_ADDRESS  (0x27)
 
-byte led_retval;
+uint8_t led_retval;
+uint8_t fan_retval;
 int16_t led_target;
+uint32_t dim_interval = 1000;
 unsigned long ptime = 0;
+
+uint8_t pin;
+
 
 void setup()
 {
-  delay(100);
+  pinMode(FAN_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(FAN_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
 
-  pinMode(FAN_PIN,OUTPUT);
-  pinMode(LED_PIN,OUTPUT);
-  digitalWrite(FAN_PIN,LOW);
-  digitalWrite(LED_PIN,LOW);
-
-  TinyWireS.begin(I2CSlaveAddress);      // Begin I2C Communication
+  TinyWireS.begin(SLAVE_ADDRESS);      // Begin I2C Communication
   TinyWireS.onReceive(led_fan);
   TinyWireS.onRequest(transmit);         // When requested, call function transmit()
 }
@@ -26,9 +40,9 @@ void setup()
 
 void loop()
 {
-  delay(50);
+  //delay(50);
   unsigned long t = millis();
-  if (t - ptime > 1000) {
+  if (t - ptime > dim_interval || t < ptime) {
     if (led_target > 0) {
       if (led_retval == 255) {
         led_retval = 255;
@@ -57,12 +71,11 @@ void led_fan(uint8_t y) {
   if (y < 1) {
     return;
   }
-//  while (TinyWireS.available()) {
   while (y--) {
     x[i] = TinyWireS.receive(); // receive byte as a character
     i++;
   }
-  uint8_t pin = x[0] & 0x0f;
+  pin = x[0] & 0x0f;
   uint8_t dim = x[0] & 0xf0;
   uint8_t value;
   if (pin == 0) {
@@ -74,14 +87,20 @@ void led_fan(uint8_t y) {
       } else {
         led_target = value - led_retval;
       }
+      if (i > 2) {
+        dim_interval = (uint32_t)x[2] * 1000;
+      }
     }
   } else if (pin == 1) {
     if (i > 1) {
       value = x[1];
       if (value > 0) {
-        digitalWrite(FAN_PIN, HIGH);
+        analogWrite(FAN_PIN, value);
+        fan_retval = value;
+        //digitalWrite(FAN_PIN, HIGH);
       } else {
         digitalWrite(FAN_PIN, LOW);
+        fan_retval = 0;
       }
     }
   }
@@ -89,7 +108,11 @@ void led_fan(uint8_t y) {
 
 void transmit()
 {
-  TinyWireS.send(I2CSlaveAddress);                 // Send last recorded distance for current sensor
+  if (pin == 0) {
+    TinyWireS.send(led_retval);
+  } else if (pin == 1) {
+    TinyWireS.send(fan_retval);    
+  }
 }
 
 
